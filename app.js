cc273df2bcbb78e689fd8ec2d5777c187f60c429
mehmet <tas349873@gmail.com>
2022-06-19 18:28:56 +0300

    The end

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const hour = document.querySelector(".hours");
+const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
 const second = document.querySelector(".second");
 const amPm = document.querySelector(".ampm");
 const date = document.querySelector(".date");
 
+const getTime  = () => {
 
-const getTime = ()=> {
-  hour.innerHTML = new Date().getHours() %12;
-  minute.innerHTML = new Date().getMinutes();
-  second.innerHTML = new Date().getSeconds();
-  date.innerHTML =  new Date().toDateString();
-   
-   if (new Date().getHours() >12) { 
-    amPm.textContent = "PM";
-    
-   } else {
-    amPm.textContent ="AM"
-   }
-   hour.textContent.length == 1 && (hour.textContent = "0" + hour.textContent);
-   minute.textContent.length == 1 && (minute.textContent = "0" + minute.textContent);
-   second.textContent.length == 1 && (second.textContent = "0" + second.textContent);
+hour.innerHTML = new Date().getHours() %12;
+minute.innerHTML = new Date().getMinutes();
+second.innerHTML = new Date().getSeconds();
+date.innerHTML = new Date().toDateString();
+
+if (new Date().getHours()>12) {
+  amPm.textContent = "PM" ; 
+} else {
+  amPm.textContent = "AM";
+}
+
+hour.innerHTML.length == 1 &&(hour.innerHTML = "0"+hour.innerHTML);
+minute.innerHTML.length == 1 &&(minute.innerHTML = "0"+minute.innerHTML);
+second.innerHTML.length == 1 &&(second.innerHTML = "0"+second.innerHTML);
+
+
 };
+
 
 setInterval(getTime,999);
